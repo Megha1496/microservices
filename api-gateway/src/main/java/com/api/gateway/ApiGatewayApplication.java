8506ccc4534c8a250e9cfad755d353b63500079f
Hitesh Gupta <hiteshgupta729@gmail.com>
2023-11-27 18:43:33 +0530

    configurations

--- api-gateway/src/main/java/com/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/api/gateway/ApiGatewayApplication.java
@@ -2,9 +2,8 @@ package com.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.config.GatewayClassPathWarningAutoConfiguration;
 
-@SpringBootApplication(exclude = GatewayClassPathWarningAutoConfiguration.class)
+@SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
